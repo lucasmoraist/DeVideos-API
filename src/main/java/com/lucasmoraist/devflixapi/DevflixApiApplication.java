64cf175624cc6493eb9f23ba2aff605077167421
lucasmoraist <luksmnt1101@gmail.com>
2024-05-15 10:58:13 -0300

    first commit

---
+++ src/main/java/com/lucasmoraist/devflixapi/DevflixApiApplication.java
@@ -0,0 +1,13 @@
+package com.lucasmoraist.devflixapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevflixApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevflixApiApplication.class, args);
+	}
+
+}
