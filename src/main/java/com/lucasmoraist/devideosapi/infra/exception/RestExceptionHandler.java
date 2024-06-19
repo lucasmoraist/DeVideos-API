fa25ac9e00ec345f256f9eecf53bb9bccf0f79d6
lucasmoraist <luksmnt1101@gmail.com>
2024-06-19 13:16:24 -0300

    chore: add more exceptions handler

--- src/main/java/com/lucasmoraist/devideosapi/infra/exception/RestExceptionHandler.java
+++ src/main/java/com/lucasmoraist/devideosapi/infra/exception/RestExceptionHandler.java
@@ -2,6 +2,8 @@ package com.lucasmoraist.devideosapi.infra.exception;
 
 import com.lucasmoraist.devideosapi.exception.ExceptionDTO;
 import com.lucasmoraist.devideosapi.exception.ResourceNotFound;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -17,4 +19,27 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         return ResponseEntity.badRequest().body(dto);
     }
 
+    @ExceptionHandler(IllegalArgumentException.class)
+    protected ResponseEntity<ExceptionDTO> illegalArgument(IllegalArgumentException e){
+        ExceptionDTO dto = new ExceptionDTO(e.getMessage(), HttpStatus.BAD_REQUEST);
+        return ResponseEntity.badRequest().body(dto);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    protected ResponseEntity<ExceptionDTO> nullPointerExceptions(DataIntegrityViolationException e){
+        ExceptionDTO dto = new ExceptionDTO("Invalid Values", HttpStatus.BAD_REQUEST);
+        return ResponseEntity.badRequest().body(dto);
+    }
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<ExceptionDTO> genericExceptions(Exception e){
+        ExceptionDTO dto = new ExceptionDTO(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        return ResponseEntity.badRequest().body(dto);
+    }
+
+    @ExceptionHandler(DataAccessException.class)
+    protected ResponseEntity<ExceptionDTO> failedConnectDatabase(DataAccessException e){
+        ExceptionDTO dto = new ExceptionDTO("Failed to connect to database", HttpStatus.BAD_GATEWAY);
+        return ResponseEntity.badRequest().body(dto);
+    }
 }
