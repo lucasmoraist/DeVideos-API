d9603f5bfa7fb9454f2b91ec5922a4ccb349b581
lucasmoraist <luksmnt1101@gmail.com>
2024-06-24 10:33:52 -0300

    feat: Add CategoryDefaultException class

---
+++ src/main/java/com/lucasmoraist/devideosapi/exception/CategoryDefaultException.java
@@ -0,0 +1,7 @@
+package com.lucasmoraist.devideosapi.exception;
+
+public class CategoryDefaultException extends RuntimeException{
+    public CategoryDefaultException(String msg) {
+        super(msg);
+    }
+}
